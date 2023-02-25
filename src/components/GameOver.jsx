c4643cc69c5e0bdfa8610d92bf2d923ff21930b1
Ray <raymondly96@gmail.com>
2023-02-25 02:06:06 -0800

    Add leaderboard saving logic and input validation

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,30 +1,95 @@
 import classNames from "classnames"
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Leaderboard from "./Leaderboard";
 import styles from '/src/scss/components/GameOver.module.scss'
 import Footer from '/src/components/Footer'
 import Register from "./Register";
+import { firestore } from '/src/firebase.js'
+import {
+  collection,
+  addDoc,
+} from 'firebase/firestore';
+import BadWordsFilter from "bad-words";
 
 export default function GameOver({setGameState, totalTimeElapsed, resetGameData, score, isLandscape}) {
+  const [isFormShown, setIsFormShown] = useState(false);
+  const [name, setName] = useState('');
   const [isSaved, setIsSaved] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [isFormShown, setIsFormShown] = useState(false);
+  const [error, setError] = useState('');
+  const registerRef = useRef(null);
+  const inputRef = useRef(null);
+  const saveBtnRef = useRef(null);
+
+  const saveUserScore = async () => {
+    const filter = new BadWordsFilter;
+    const docRef = await addDoc(collection(firestore, 'scores'), {
+      name: filter.clean(name),
+      score: score,
+      time: totalTimeElapsed
+    })
+    console.log('Saved to db: ', docRef);
+  }
+
+  useEffect(() => {
+    const handleClickEvent = (e) => {
+      if(isFormShown 
+          && registerRef.current 
+          && !registerRef.current.contains(e.target)
+          && saveBtnRef.current
+          && !saveBtnRef.current.contains(e.target)) {
+        setIsFormShown(false);
+      }
+    }
+    document.addEventListener('click', handleClickEvent);
+
+    return () => document.removeEventListener('click', handleClickEvent);
+  }, [isFormShown])
+
+  useEffect(() => {
+    if (name.length <= 20)
+      setError('');
+  }, [name])
 
   const handleClickTryAgain = () => {
     setGameState('Ready');
     resetGameData();
   }
 
-  const handleClickShowForm = () => {
-    setIsFormShown((prev) => !prev);
+  const handleClickSave = async (e) => {
+    e.preventDefault();
+    if (isFormShown) {
+      // Basic input validation
+      if (name.length > 20) {
+        setError('Name must be 20 characters or less');
+        return;
+      }
+
+      inputRef.current.blur();
+      setName('');
+      setIsFormShown(false);
+      setIsSaved(true);
+
+      try {
+        await saveUserScore();
+      } catch (error) {
+        console.error("Problem saving score data to database", error);
+      }      
+    } else {
+      inputRef.current.focus();
+      setIsFormShown(true);
+    }
   }
 
   return (
     <div className={classNames(styles.gameOverGrid)}>
       <div className={classNames(styles.display)}>
-        <div>
-          You solved {score} {score === 1 ? 'puzzle' : 'puzzles'} in {totalTimeElapsed} seconds!
-        </div>
+        {(error && isFormShown)
+          ? <div className={classNames(styles.error)}>{error}</div>
+          : <div>
+              You solved {score} {score === 1 ? 'puzzle' : 'puzzles'} in {totalTimeElapsed} seconds!
+            </div>
+        }
         <div>
           <button 
             onClick={handleClickTryAgain}
@@ -32,12 +97,27 @@ export default function GameOver({setGameState, totalTimeElapsed, resetGameData,
           >
             Try Again
           </button>
-          <button 
-            onClick={handleClickShowForm}
-            className={classNames(styles.btn)}
-          >
-            Save Score
-          </button>
+          {!isSaved
+            ? <button 
+                onClick={handleClickSave}
+                className={classNames(
+                            styles.btn,
+                            isFormShown && styles.active,
+                            {'pulse': isFormShown && !error},
+                            error && isFormShown && styles.btnDisabled
+                          )}
+                ref={saveBtnRef}
+              >
+                Save Score
+              </button>
+            : <button
+                className={classNames(styles.btnSaved)}
+                disabled
+              >
+                Saved!
+              </button>
+          }
+          
         </div>
       </div>
       <Leaderboard />
@@ -46,6 +126,15 @@ export default function GameOver({setGameState, totalTimeElapsed, resetGameData,
       }
       <Register 
         isFormShown={isFormShown}
+        name={name}
+        setName={setName}
+        isSaved={isSaved}
+        setIsSaved={setIsSaved}
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
+        inputRef={inputRef}
+        registerRef={registerRef}
+        handleClickSave={handleClickSave}
       />
     </div>
   )
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,19 +1,30 @@
 import classNames from "classnames"
 import styles from '/src/scss/components/Register.module.scss'
 
-export default function Register({isFormShown}) {
+export default function Register({isFormShown, name, setName, isSaved, setIsSaved,
+                                  isLoading, setIsLoading, inputRef, registerRef,
+                                  handleClickSave
+                                }) {
+
   return (
     <div 
       className={classNames(
         styles.register,
         {'slideIn': isFormShown}
       )}
+      ref={registerRef}
     >
       <div className={classNames(styles.formContainer)}>
-        <span>Enter your name: </span>
-        <input 
-          className={classNames(styles.pill)}
-        />
+        <form onSubmit={handleClickSave}>
+          <span>Enter your name: </span>
+          <input 
+            className={classNames(styles.pill)}
+            autoFocus
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            ref={inputRef}
+          />
+        </form>
       </div>
     </div>
   )
