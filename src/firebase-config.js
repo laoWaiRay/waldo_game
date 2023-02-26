f3afecc22c54671ec17bee0038605233a748be81
Ray <raymondly96@gmail.com>
2023-02-26 14:03:47 -0800

    update firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiKey: import.meta.env.VITE_apiKey,
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "waldogame-dda5b.firebaseapp.com",
   projectId: "waldogame-dda5b",
   storageBucket: "waldogame-dda5b.appspot.com",
@@ -8,11 +8,12 @@ const config = {
 };
 
 export function getFirebaseConfig() {
-  if (!config || !config.apiKey) {
-    if (!config.apiKey)
+  if (!config.apiKey)
       console.log('no api key');
     if (!config)
       console.log('no config')
+      
+  if (!config || !config.apiKey) {
     throw new Error('No Firebase configuration object provided.' + '\n' +
     'Add your web app\'s configuration object to firebase-config.js');
   } else {
